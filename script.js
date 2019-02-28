3e72890d5afc46d215fe962ca9c8d7bd012c9ba3
DevEngberg <38111502+DevEngberg@users.noreply.github.com>
2019-02-27 21:18:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+    var mainDiv = $('#mainDiv');
+    var searchTerm = $('<div>');
+    var searchParameters = $('<div><h2>searchParameters<div id = "parameters">');
+    $ (searchParameters).append(mainDiv);
